cd5dfc8626d3778d66ce35802ba3d95b4ff0eb10
craigwann <craigwann@me.com>
2022-03-01 16:00:21 -0800

    update js input form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $(#formOne).submit(function() {
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput = $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
     $(".person1").append("blah blah");
     $(".person2").append("blah blah");
     $(".animal").append("blah blah");
@@ -7,7 +14,9 @@ $(document).ready(function() {
     $(".verb").append("blah blah");
     $(".noun").append("blah blah");
 
-    $(#story).show();
-  })
+    $("#story").show();
 
-})
+    event.preventDefault();
+  });
+
+});
